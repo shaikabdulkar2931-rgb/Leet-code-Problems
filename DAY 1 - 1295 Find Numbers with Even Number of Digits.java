608925c9b3610da2c7722474407835f789d30de5
shaikabdulkar2931-rgb <shaikabdulkar2931@gmail.com>
2026-07-16 11:06:54 +0530

    Update DAY 1 - 1295 Find Numbers with Even Number of Digits.java

--- DAY 1 - 1295 Find Numbers with Even Number of Digits.java
+++ DAY 1 - 1295 Find Numbers with Even Number of Digits.java
@@ -3,14 +3,14 @@ lass Solution {
         int evenDigitCount = 0;
         
         for (int num : nums) {
-            // Count the number of digits for the current number
+            
             int digits = 0;
             while (num > 0) {
                 digits++;
                 num /= 10;
             }
             
-            // Check if the digit count is even
+            
             if (digits % 2 == 0) {
                 evenDigitCount++;
             }
