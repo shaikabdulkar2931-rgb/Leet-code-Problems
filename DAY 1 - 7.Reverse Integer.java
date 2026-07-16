77d3eae31b18c5892369b02b372702ac6c3aeb7b
shaikabdulkar2931-rgb <shaikabdulkar2931@gmail.com>
2026-07-16 11:18:36 +0530

    Update DAY 1 - 7.Reverse Integer.java

--- DAY 1 - 7.Reverse Integer.java
+++ DAY 1 - 7.Reverse Integer.java
@@ -3,7 +3,7 @@ class Solution {
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
-            x /= 10;
+        x /= 10;
          if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
                 return 0;
             }
