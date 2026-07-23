ebbf40b945fce135d17121b55ae9ee30fc4436e1
Sk Abdul kareem <shaikabdulkar2931@gmail.com>
2026-07-23 14:22:25 +0530

    3658.GCD of Odd and Even Sums.java

--- 3658.GCD of Odd and Even Sums.java
+++ 3658.GCD of Odd and Even Sums.java
@@ -1,5 +1,12 @@
 class Solution {
+
+    private int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
     public int gcdOfOddEvenSums(int n) {
-     return n;   
+        return gcd(n * n, n * (n + 1));
     }
 }
