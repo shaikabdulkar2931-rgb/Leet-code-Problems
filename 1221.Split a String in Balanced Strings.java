55f3179c2d0ba5a8ae3d2dc01bc897d982834deb
Sk Abdul kareem <shaikabdulkar2931@gmail.com>
2026-07-27 13:56:01 +0530

    1221.Split a String in Balanced Strings.java

---
+++ 1221.Split a String in Balanced Strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0;
+        int balance = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if (c == 'L') {
+                balance++;
+            } else {
+                balance--;
+            }
+
+            if (balance == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
